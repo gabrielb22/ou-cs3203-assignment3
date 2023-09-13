9c9e5b41dfaed12ea0e7384cb165027e7ce73f64
gabrielb22 <gabriel.p.browning-1@ou.edu>
2023-09-13 16:52:29 -0500

    added Product Function

--- SE1.cpp
+++ SE1.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//comment for testing commit
-//additional comment for further testing
+
 int sum(int numbers[], int length){ //adds all the integers in an array
 	int temp = 0;
 	for(int i = 0; i < length; i++){
@@ -11,11 +10,20 @@ int sum(int numbers[], int length){ //adds all the integers in an array
 	return temp;
 }
 
+int product(int numbers[], int length) { //multiplies all the ints in an array
+	int temp = 1;
+	for(int i = 0; i < length; i++){
+		temp = temp*numbers[i];
+	}
+
+	return temp;
+}
+
 
 int main(){
-	int numbers[] = {1, 2, 3, 4, 5, 10};
+	int numbers[] = {1, 2, 3, 4};
 
 	cout << "Sum: " << sum(numbers, (sizeof(numbers) / sizeof(*numbers))) << endl;
-
+	cout << "Product: " << product(numbers, (sizeof(numbers) / sizeof(*numbers))) << endl;
 	return 0;
 }

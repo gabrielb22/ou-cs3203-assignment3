bbc25c3f8e3d7b5ec56505dc4b8fab3454180112
gabrielb22 <gabriel.p.browning-1@ou.edu>
2023-09-13 15:12:47 -0500

    Create SE1.cpp
    Initial Commit

---
+++ SE1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int sum(int numbers[], int length){ //adds all the integers in an array
+	int temp = 0;
+	for(int i = 0; i < length; i++){
+		temp = temp + numbers[i];
+	}
+	return temp;
+}
+
+
+int main(){
+	int numbers[] = {1, 2, 3, 4, 5, 10};
+
+	cout << "Sum: " << sum(numbers, (sizeof(numbers) / sizeof(*numbers))) << endl;
+
+	return 0;
+}

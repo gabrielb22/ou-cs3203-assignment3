a5b4b11498cddae03bb4573d90f4936225e01986
gabrielb22 <60167555+gabrielb22@users.noreply.github.com>
2023-09-13 23:07:23 -0500

    part 6

--- SE1.cpp
+++ SE1.cpp
@@ -27,9 +27,14 @@ void flip(int numbers[], int temp[], int length){
 }
 
 int main(){
-	int numbers[] = {1, 2, 3, 4};
-	int temp[sizeof(numbers) / sizeof(*numbers)];
+	int numbers[4];
+	int temp[4];
 
+	cout<<"Enter 4 numbers: " << endl;
+	for(int i = 0; i<4; i++){
+		cout << "Number: " << i + 1 << endl;
+		cin >> numbers[i];
+	}
 	cout << "Sum: " << sum(numbers, (sizeof(numbers) / sizeof(*numbers))) << endl;
 	cout << "Product: " << product(numbers, (sizeof(numbers) / sizeof(*numbers))) << endl;
 
@@ -39,7 +44,6 @@ int main(){
 		cout << temp[i] << " ";
 	}
 	cout << endl;
-
 	return 0;
 }
 
